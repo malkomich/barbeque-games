c7e2f420e984222eda47b31eea2ae755de276207
Juan Carlos González Cabrero <malkomich@gmail.com>
2017-08-29 00:45:10 +0200

    Implement logic of checkboxes and play rounds

--- src/components/Dashboard/Views/VideoGame.vue
+++ src/components/Dashboard/Views/VideoGame.vue
@@ -1,37 +1,48 @@
 <template>
-  <v-card class="grey lighten-4">
+  <div>
+    <v-card class="grey lighten-4" v-if="currentVideoStage">
 
-    <v-card-media>
-      <div class="content">
-        <google-youtube chromeless
-          :video-id="currentVideoStage.videoId"
-          autoplay="1"
-          height="270px"
-          width="480px"
-          rel="0"
-          start="5">
-        </google-youtube>
-      </div>
-    </v-card-media>
+      <v-card-media>
+        <div class="content">
+          <google-youtube chromeless
+            :video-id="currentVideoStage.videoId"
+            autoplay="1"
+            height="270px"
+            width="480px"
+            rel="0"
+            start="5">
+          </google-youtube>
+        </div>
+      </v-card-media>
 
-    <v-card-text>
-      <div v-for="option in currentVideoStage.options" :key="option.text">
-        <paper-checkbox :checked="option.text === selectedOptionText" :change="onOptionChanged(option)">
-          {{ option.text }}
-        </paper-checkbox>
-      </div>
-    </v-card-text>
+      <v-card-text>
+        <div v-for="option in currentVideoStage.options" :key="option.text">
+          <paper-checkbox :checked="option.checked" @change="onOptionChanged(option)" :disabled="roundFinished"
+              :class="{ 'highlight-error': !option.right && roundFinished && option.checked,
+                        'highlight-success': option.right && roundFinished }">
+            {{ option.text }}
+          </paper-checkbox>
+        </div>
+      </v-card-text>
 
-    <v-card-actions>
-      <v-btn flat class="teal--text" @click="submitAnswer()">Dale!</v-btn>
-      <v-btn flat class="grey--text" @click="skip()">No me la juego...</v-btn>
-    </v-card-actions>
+      <v-card-actions>
+        <v-btn flat class="teal--text" @click.native="submitAnswer()">Dale!</v-btn>
+        <v-btn flat class="grey--text" @click.native="skip()">No me la juego...</v-btn>
+      </v-card-actions>
 
-  </v-card>
+    </v-card>
+
+    <v-card class="grey lighten-4" v-if="!currentVideoStage">
+      <v-card-text>
+        There are no videos left. Sorry!
+      </v-card-text>
+    </v-card>
+  </div>
 </template>
 
 <script>
   import VideoStage from 'src/domain/video-stage';
+  import Option from 'src/domain/option';
   
   // Polymer youtube player component
   import 'google-youtube/google-youtube.html';
@@ -44,60 +55,50 @@
         loading: false,
         videoStages: [
           new VideoStage('GXvSn0YxjR8', 0, [
-            { text: 'Aparece un hombre gritando con un aire acondicionado', right: true },
-            { text: 'La mujer es arrollada por un dinosaurio hasta la piscina', right: false },
-            { text: 'Al quitarse la camisa, aparece un pecho lleno de pelo', right: false },
-            { text: 'En realidad es un anuncio de wonderbra', right: false },
+            new Option('Aparece un hombre gritando con un aire acondicionado', true),
+            new Option('La mujer es arrollada por un dinosaurio hasta la piscina'),
+            new Option('Al quitarse la camisa, aparece un pecho lleno de pelo'),
+            new Option('En realidad es un anuncio de wonderbra'),
           ]),
           new VideoStage('96paLghfXY8', 0, [
-            { text: 'Le arroya un grupo de ciclistas', right: true },
-            { text: 'Cae uno de los árboles de alrededor encima de él mientras baila', right: false },
-            { text: 'Al hacer un paso de baile cae contra el suelo, perdiendo varios dientes',
-              right: false },
-            { text: 'La cámara aleja el zoom y se descubre que es una coreografía grupal a gran escala',
-              right: false },
+            new Option('Le arroya un grupo de ciclistas', true),
+            new Option('Cae uno de los árboles de alrededor encima de él mientras baila'),
+            new Option('Al hacer un paso de baile cae contra el suelo, perdiendo varios dientes'),
+            new Option('La cámara aleja el zoom y se descubre que es una coreografía grupal a gran escala'),
           ]),
           new VideoStage('kwtWTpLYvOo', 0, [
-            { text: 'Empieza a violar al globo hasta que explota', right: false },
-            { text: 'Empiezan a aparecen muchos globos iguales y el conejo sale huyendo',
-              right: false },
-            { text: 'El globo explota, aturdiendo al conejo, y acto después se traga el globo explotado',
-              right: false },
-            { text: 'Aparece repentinamente un pájaro, llevándose al conejo en el aire', right: false },
+            new Option('Empieza a violar al globo hasta que explota', true),
+            new Option('Empiezan a aparecen muchos globos iguales y el conejo sale huyendo'),
+            new Option('El globo explota, aturdiendo al conejo, y acto después se traga el globo explotado'),
+            new Option('Aparece repentinamente un pájaro, llevándose al conejo en el aire'),
           ]),
           new VideoStage('LcNmFFy0Ng4', 0, [
-            { text: 'Tira el monopatín y se desliza calle abajo sólo con sus playeras', right: true },
-            { text: 'Hay un trozo de suelo falso y se cae dentro', right: false },
-            { text: 'Aparece un hombre disfrazado de pollo y le hace un placaje', right: false },
-            { text: 'Al intentar subirse, cae con la cabeza en la tabla y la parte', right: false },
+            new Option('Tira el monopatín y se desliza calle abajo sólo con sus playeras', true),
+            new Option('Hay un trozo de suelo falso y se cae dentro'),
+            new Option('Aparece un hombre disfrazado de pollo y le hace un placaje'),
+            new Option('Al intentar subirse, cae con la cabeza en la tabla y la parte'),
           ]),
           new VideoStage('h_hcjTdiNvk', 0, [
-            { text: 'Hay una bella mujer haciendo una felación al hombre enano', right: true },
-            { text: 'Al arrancar, el motor del coche del hombre enano tiene mayor potencia y le adelanta',
-              right: false },
-            { text: 'Aparece de repente un coro de ópera alrededor del coche deportivo', right: false },
-            { text: 'El hombre enano enseña su placa de policía, y detiene al conductor del deportivo',
-              right: false },
+            new Option('Hay una bella mujer haciendo una felación al hombre enano', true),
+            new Option('Al arrancar, el motor del coche del hombre enano tiene mayor potencia y le adelanta'),
+            new Option('Aparece de repente un coro de ópera alrededor del coche deportivo'),
+            new Option('El hombre enano enseña su placa de policía, y detiene al conductor del deportivo'),
           ]),
           new VideoStage('mxIlyP-Sxw4', 0, [
-            { text: 'Saca un arma y empieza a disparar al interior del buzón', right: true },
-            { text: 'El buzón se despega del suelo y sale huyendo', right: false },
-            { text: 'Empiezan a salir un chorro potente de agua del buzón', right: false },
-            { text: 'El hombre tira una cerilla encendida al interior del buzón', right: false },
+            new Option('Saca un arma y empieza a disparar al interior del buzón', true),
+            new Option('El buzón se despega del suelo y sale huyendo'),
+            new Option('Empiezan a salir un chorro potente de agua del buzón'),
+            new Option('El hombre tira una cerilla encendida al interior del buzón'),
           ]),
         ],
+        roundFinished: false,
         selectedOption: null,
       };
     },
     computed: {
       currentVideoStage: {
         get() {
-          return this.videoStages.pop();
-        },
-      },
-      selectedOptionText: {
-        get() {
-          return this.selectedOption ? this.selectedOption.text : '';
+          return this.videoStages.length ? this.videoStages[0] : undefined;
         },
       },
     },
@@ -106,24 +107,49 @@
     },
     methods: {
       submitAnswer() {
-        if (!this.selectedOption) {
+        if (this.roundFinished) {
           return;
         }
-        const resultMessage = this.selectedOption.right ? 'CORRECTO!!' : 'HAS FALLADO';
-        console.log(resultMessage, this.selectedOption.text);
-        // TODO: Highlight right answer and continue playing video
+        if (!this.selectedOption) {
+          this.notifyVue('You must select any option!', 'warning');
+          return;
+        }
+        if (this.selectedOption.right) {
+          this.notifyVue('CORRECTO!', 'success');
+        } else {
+          this.notifyVue('HAS FALLADO!', 'danger');
+        }
+        this.roundFinished = true;
+        setTimeout(() => {
+          this.next();
+        }, 3000);
       },
-      skip() {
-        // TODO: Remove current video from array
+      next() {
+        this.videoStages.shift();
+        this.roundFinished = false;
+        this.selectedOption = null;
       },
-      onOptionChanged(option) {
-        this.selectedOption = option;
+      onOptionChanged(selectedOption) {
+        selectedOption.check(true);
+        this.selectedOption = selectedOption;
+        this.currentVideoStage.options
+          .filter(option => option.text !== selectedOption.text)
+          .forEach(option => option.check(false));
       },
       shuffleVideoStages() {
         this.videoStages.sort(() => 0.5 - Math.random());
       },
       onVideoTimeChanged(event) {
         console.log('Current time changed', event);
+      },
+      notifyVue(message, type) {
+        this.$notifications.notify({
+          message,
+          icon: 'ti-gift',
+          horizontalAlign: 'bottom',
+          verticalAlign: 'center',
+          type,
+        });
       },
     },
   };
@@ -132,11 +158,17 @@
 <style>
   paper-checkbox {
     --paper-checkbox-size: 2em;
-    --paper-checkbox-checked-color: var(--paper-red-500);
-    --paper-checkbox-checked-ink-color: var(--paper-red-500);
-    --paper-checkbox-unchecked-color: var(--paper-red-900);
-    --paper-checkbox-unchecked-ink-color: var(--paper-red-900);
-    --paper-checkbox-label-color: var(--paper-red-500);
+    --paper-checkbox-checked-color: var(--paper-purple-A400);
+    --paper-checkbox-checked-ink-color: var(--paper-purple-200);
+    --paper-checkbox-unchecked-color: var(--paper-teal-300);
+    --paper-checkbox-unchecked-ink-color: var(--paper-purple-100);
+    --paper-checkbox-label-color: var(--paper-indigo-800);
     --paper-checkbox-vertical-align: top;
+  }
+  paper-checkbox.highlight-error {
+    background-color: #F8E0E0;
+  }
+  paper-checkbox.highlight-success {
+    background-color: #CEF6CE;
   }
 </style>
---
+++ src/domain/option.js
@@ -0,0 +1,13 @@
+export default class Option {
+
+  constructor(text, right, checked) {
+    this.text = text;
+    this.right = right || false;
+    this.checked = checked || false;
+  }
+
+  check(checked) {
+    this.checked = checked;
+  }
+
+}
